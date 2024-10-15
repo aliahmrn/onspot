fc9ac926e82b3c353ab18803148b5ce79bd3fed7
Husna <116620275+iNinaLy@users.noreply.github.com>
2024-10-15 10:11:38 +0800

    working login

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-  final String baseUrl = 'http://10.0.2.2:8000/public/api'; // Android Emulator
+  final String baseUrl = 'http://10.0.2.2:8000/api'; // Android Emulator
 
   // Login function for officers
   Future<void> login(String input, String password) async {
